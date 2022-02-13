5e14e3b1a14f22d3f9622f5f2e84f867e324abf5
RFadhil <58224930+Gonken-GN@users.noreply.github.com>
2022-02-13 19:26:29 +0700

    Update index.php

--- Per1/Latihan B/php/index.php
+++ Per1/Latihan B/php/index.php
@@ -1,7 +1,7 @@
 <?php
 include "tim.php";
 
-$sepakbola = new tim("Persib", "Indonesia", "2000", "Asep", "Bambang");
+$sepakbola = new tim("Persib", "Indonesia", "2000", "Asep", "Bambang, Todo, George, Uji");
 
 echo "Nama Tim: " .$sepakbola->getNama() ."<br>";
 echo "Negara Tim: " .$sepakbola->getNegara() ."<br>";
